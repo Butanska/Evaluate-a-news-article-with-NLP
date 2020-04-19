fb0d8a8340bfbb09a7deda5a428a1bdf258d97ec
Butanska <romi_savate@yahoo.com>
2020-04-20 01:24:29 +0300

    enter

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,4 @@
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
@@ -42,6 +43,7 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        })
+        }),
+        new WorkboxPlugin.GenerateSW(),
     ]
 }
